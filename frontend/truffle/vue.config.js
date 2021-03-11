8f629634bd97269b346662a4133821fd991b76e3
윤예준 <dbm01049@gmail.com>
2021-03-11 15:31:11 +0900

    Create Vue-Cli 시작프로젝트생성(프론트 포트번호:8000, eslint경고 해제)

---
+++ frontend/truffle/vue.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  lintOnSave: false,
+  productionSourceMap: false, //빌드 시에 webpack 내의 코드가 보이지 않도록 설정
+  filenameHashing: false, //빌드 시에 파일 이름에 해쉬값이 없도록 함
+  publicPath: '', // 디폴트가 '/' 인데, root 호스트 중심으로 탐색하므로 상대경로로 갈 수 있도록 publicPath를 공백으로 설정
+  // 개발 서버 설정
+  devServer: {
+    hot: true, // 바뀐 모듈만 리로드
+    // 프록시 설정
+    proxy: {
+      // 프록시 요청을 보낼 api의 시작 부분
+      '/api': {
+        // 프록시 요청을 보낼 서버의 주소
+        target: 'http://localhost:8080',
+      },
+    },
+  },
+  css: {
+    loaderOptions: {
+      css: {
+        sourceMap: process.env.NODE_ENV !== 'production' ? true : false,
+      },
+    },
+  },
+};
