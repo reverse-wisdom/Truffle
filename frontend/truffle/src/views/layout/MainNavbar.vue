ad3c7214bd2c46a517c8703c88dc02669c3ac62b
이원회 <doggodi303@naver.com>
2021-03-23 22:56:55 +0900

    Create S04P23D110-80 : 게시판 CRUD  작성

--- frontend/truffle/src/views/layout/MainNavbar.vue
+++ frontend/truffle/src/views/layout/MainNavbar.vue
@@ -7,6 +7,8 @@
     <router-link to="/signin-signup">signin-signup</router-link>
     |
     <router-link to="/landing">FullPage</router-link>
+    |
+    <router-link to="/board">board</router-link>
   </div>
 </template>
 
