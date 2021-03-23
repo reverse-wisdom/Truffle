783c756f68f44e2a693c87a1b1bd5507904ac6bb
reverse-wisdom <pumpkintime84@gmail.com>
2021-03-23 21:50:10 +0900

    Add S04P23D110-61: 네이버 로그인 적용했으나 실패 & 메인페이지 UI 작업중

--- frontend/truffle/src/store/index.js
+++ frontend/truffle/src/store/index.js
@@ -1,15 +1,14 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue';
+import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    SERVER_URL: 'http://j4d110.p.ssafy.io/',
+    LOCAL_URL: 'http://localhost:8081',
   },
-  getters: {
-
-  },
-  mutations: {
-  },
+  getters: {},
+  mutations: {},
   actions: {},
 });
