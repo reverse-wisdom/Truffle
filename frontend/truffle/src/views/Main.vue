901bbcd309fcdbc04688e7b10e2c5874a9751c99
leehyeji <leehyeji11@naver.com>
2021-03-24 22:03:09 +0900

    Update S04P23D110-72: 메인페이지 UI 수정

--- frontend/truffle/src/views/Main.vue
+++ frontend/truffle/src/views/Main.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="bg">
-    <div class="container">
-      <h1>truffle</h1>
+  <div>
+    <div class="container projects">
+      <h1>TRUFFLE</h1>
+      <div class="detail">쇼핑에 재미와 신뢰를 더하다</div>
+      <div class="overlay"></div>
     </div>
   </div>
 </template>
@@ -11,32 +13,111 @@ export default {};
 </script>
 
 <style scoped>
-.bg {
-  height: 100%;
-  width: 100%;
-  background-color: #6b6b6b;
-}
-.container {
+* {
   margin: 0;
   padding: 0;
-  font-family: 'Montserrat', sans-serif;
-  /* background-color: #333; */
+  /* box-sizing: border-box; */
 }
-h1 {
-  font-size: 200px;
-  text-transform: uppercase;
-  font-weight: 900;
-  letter-spacing: 20px;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+/* .projects {
+  margin: 0 !important;
+} */
+.container {
+  min-height: 100vh;
+  position: inherit;
+  width: auto;
   margin: 0;
-  background-image: url('../assets/img/mainbg.png');
-  background-size: cover;
-  background-color: rgba(255, 255, 255, 0.8);
-  color: rgba(255, 255, 255, 0.5);
-  /* -webkit-text-fill-color: transparent; */
-  background-clip: text;
+  display: flex;
+  background-color: #c2f929;
+  justify-content: center;
+  align-items: center;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+
+h1 {
+  font-size: 10rem;
+  color: #fff;
+  transform: translateY(-600px);
+  animation: 2s slideIn ease-in-out forwards 2s;
+  z-index: 10;
+  opacity: 0;
+  letter-spacing: 20px;
+  position: relative;
+}
+.detail {
+  font-size: 1rem;
+  color: #fff;
+  transform: translateY(600px);
+  animation: 3s slideIn ease-in-out forwards 3s;
+  z-index: 10;
+  opacity: 0;
+  letter-spacing: 10px;
+  position: relative;
+}
+
+/* h1::before {
+  content: '';
+  width: 0%;
+  height: 76px;
+  background-color: #0f21bd;
+  position: absolute;
+  bottom: -10px;
+  animation: 1s underline ease-in-out forwards 1s;
+  mix-blend-mode: screen;
+} */
+
+.overlay {
+  position: absolute;
+  width: 100%;
+  top: 0;
+  bottom: 0;
+  opacity: 0;
+  left: 0;
+  right: 0;
+  background-color: #000;
+  transform: scale(0.5);
+  animation: 2s slideIn ease-in-out forwards, 2s skewBg ease-in-out;
+}
+
+.anim-typewriter {
+  animation: typewriter 4s steps(40) 1s 1 normal both, blinkTextCursor 500ms steps(40) infinite normal;
+}
+
+@keyframes typewriter {
+  from {
+    width: 0;
+  }
+  to {
+    width: 16em;
+  }
+}
+
+@keyframes blinkTextCursor {
+  from {
+    border-right-color: rgba(255, 255, 255, 0.75);
+  }
+  to {
+    border-right-color: transparent;
+  }
+}
+@keyframes skewBg {
+  0% {
+    transform: scale(0.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+
+@keyframes underline {
+  100% {
+    width: 100%;
+  }
+}
+
+@keyframes slideIn {
+  100% {
+    transform: translateY(0px);
+    opacity: 1;
+  }
 }
 </style>
