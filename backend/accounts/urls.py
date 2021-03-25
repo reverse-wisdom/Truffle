ffcf9775909c8d22bda94c73d129b84457b79539
이창근 <sieglcg@gmail.com>
2021-03-25 23:06:11 +0900

    Create 휴대폰 SMS 본인인증

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -6,6 +6,7 @@ from rest_framework_jwt.views import obtain_jwt_token
 app_name = 'accounts'
 
 urlpatterns = [
-    path('signup/', views.signup, name='signup'),
-    
+    path('signup/', views.Signup.as_view({'post': 'signup'}), name='signup'),
+    path('verify_phone/', views.Verify_phone.as_view({'post': 'post', 'get': 'get'}), name='verify_phone'),
+    path('login/', obtain_jwt_token)
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -3,23 +3,51 @@ from django.contrib.auth import get_user_model
 
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from rest_framework.views import APIView
 
-from .models import User
-from .serializers import UserSerializer
+from drf_yasg.utils import swagger_auto_schema
+
+from .models import User, Auth_Sms
+from .serializers import SignupSerializer, AuthSmsSerializer, AuthSmsQuerySerializer
 # Create your views here.
 
-@api_view(['POST'])
-def signup(request):
-    password = request.data.get('password')
+class Signup(viewsets.ModelViewSet):
+    serializer_class = SignupSerializer
 
-    serializer = UserSerializer(data=request.data)
+    def signup(self, request):
+        password = request.data.get('password')
 
-    if serializer.is_valid(raise_exception=True):
-        user = serializer.save()
-        user.set_password(password)
-        user.save()
+        serializer = SignupSerializer(data=request.data)
 
-    return Response(serializer.data, status=status.HTTP_201_CREATED)
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.save()
+            user.set_password(password)
+            user.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class Verify_phone(viewsets.ModelViewSet):
+    serializer_class = AuthSmsSerializer
+
+    def post(self, request):
+        try:
+            p_num = request.data['phone_number']
+        except KeyError:
+            return Response({'message': 'Bad Request'}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            Auth_Sms.objects.update_or_create(phone_number=p_num)
+            return Response({'message': 'OK'})
+    
+    @swagger_auto_schema(query_serializer=AuthSmsQuerySerializer)
+    def get(self, request):
+        try:
+            p_num = request.query_params['phone_number']
+            a_num = request.query_params['auth_number']
+        except KeyError:
+            return Response({'message': 'Bad Request'}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            result = Auth_Sms.check_auth_number(p_num, a_num)
+            return Response({'message': result})
