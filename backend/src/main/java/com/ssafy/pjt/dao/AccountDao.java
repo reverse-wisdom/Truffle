0386f1ae0e276765d3aa1a26e1634f75ed8a6140
윤예준 <dbm01049@gmail.com>
2021-04-03 00:12:39 +0900

    Update  S04P23D110-68 회원이메일로 당첨기록 조회/회원이메일로 진행중인 참여조회

--- backend/src/main/java/com/ssafy/pjt/dao/AccountDao.java
+++ backend/src/main/java/com/ssafy/pjt/dao/AccountDao.java
@@ -3,6 +3,7 @@ package com.ssafy.pjt.dao;
 import java.sql.SQLException;
 
 import com.ssafy.pjt.dto.AccountDto;
+import com.ssafy.pjt.dto.EventDto;
 import com.ssafy.pjt.dto.LoginRequestDto;
 
 public interface AccountDao {
@@ -15,4 +16,8 @@ public interface AccountDao {
 	public int delete(String email) throws SQLException;
 
 	public int update(AccountDto accountDto) throws SQLException;
+
+	public EventDto selectEventWinnerByEmail(String email) throws SQLException;
+
+	public EventDto selectEventParticipationByEmail(String email) throws SQLException;
 }
