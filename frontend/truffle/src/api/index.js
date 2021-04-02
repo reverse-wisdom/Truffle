128e9b3b4ce66c59da33921cf8284aadd9bd506b
이원회 <doggodi303@naver.com>
2021-04-02 23:02:46 +0900

    Update S04P23D110-30 : 회원가입 백 데이터연결

--- frontend/truffle/src/api/index.js
+++ frontend/truffle/src/api/index.js
@@ -3,17 +3,16 @@ import { setInterceptors } from './common/interceptors';
 
 function createInstance() {
   return axios.create({
-    // baseURL: process.env.VUE_APP_API_URL,
+    baseURL: 'https://j4d110.p.ssafy.io/truffle',
   });
 }
 
 function createInstanceWithAuth(url) {
   const instance = axios.create({
-    // baseURL: `${process.env.VUE_APP_API_URL}${url}`,
+    baseURL: 'https://j4d110.p.ssafy.io/truffle',
   });
   return setInterceptors(instance);
 }
-
 
 export const instance = createInstance();
 export const posts = createInstanceWithAuth();
