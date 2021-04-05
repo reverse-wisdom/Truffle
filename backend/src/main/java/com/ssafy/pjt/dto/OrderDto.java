09b2dddcd15e204b2a7b581d7df4d0ba4fa906e5
윤예준 <dbm01049@gmail.com>
2021-04-05 19:25:42 +0900

    Create S04P23D110-75 ordercontorller 구축, paymentByImpUid(imp_uid 로 검증 진행), completePayment(결제완료) 메소드 구현

--- backend/src/main/java/com/ssafy/pjt/dto/OrderDto.java
+++ backend/src/main/java/com/ssafy/pjt/dto/OrderDto.java
@@ -1,14 +1,13 @@
 package com.ssafy.pjt.dto;
 
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
 public class OrderDto {
 	int order_id;
 	int uuid;
 	int event_id;
 	int ship_status;
 	int pay_status;
+	String imp_uid; 
 }
