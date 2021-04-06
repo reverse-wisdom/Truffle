9535da1ab7a2e4f91cd12842c9c4b5ebf8ec6b2a
윤예준 <dbm01049@gmail.com>
2021-04-06 22:07:43 +0900

    Create S04P23D110-75 order contorller: updateOrderStatus 메소드 구현

--- backend/src/main/java/com/ssafy/pjt/dao/OrderDao.java
+++ backend/src/main/java/com/ssafy/pjt/dao/OrderDao.java
@@ -3,6 +3,7 @@ package com.ssafy.pjt.dao;
 import java.sql.SQLException;
 
 import com.ssafy.pjt.dto.OrderDto;
+import com.ssafy.pjt.dto.OrderUpdateRequestDto;
 
 public interface OrderDao {
 	public int completePayment(OrderDto orderDto) throws SQLException;
@@ -10,5 +11,7 @@ public interface OrderDao {
 	public OrderDto selectOrderByEventId(int event_id) throws SQLException;
 	
 	public int deleteOrderByEventId(int event_id) throws SQLException;
+	
+	public int updateOrderStatus(OrderUpdateRequestDto orderUpdateRequestDto) throws SQLException;
 
 }
---
+++ backend/src/main/java/com/ssafy/pjt/dto/OrderUpdateRequestDto.java
@@ -0,0 +1,11 @@
+package com.ssafy.pjt.dto;
+
+import lombok.Data;
+
+@Data
+public class OrderUpdateRequestDto {
+	int event_id;
+	int ship_status;
+	int pay_status;
+
+}
