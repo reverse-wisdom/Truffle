a7bd45f6feed90cfc41fad8d301ad61a71cbb8dd
윤예준 <dbm01049@gmail.com>
2021-04-08 09:27:16 +0900

    Update Nginx 리퀘스트헤더 크기설정

--- frontend/truffle/vue.config.js
+++ frontend/truffle/vue.config.js
@@ -11,9 +11,9 @@ module.exports = {
     // 프록시 설정
     proxy: {
       // 프록시 요청을 보낼 api의 시작 부분
-      '/api': {
+      '/truffle': {
         // 프록시 요청을 보낼 서버의 주소
-        target: 'http://localhost:8080',
+        target: 'http://localhost:8000',
       },
     },
   },
