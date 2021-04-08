1ca637a380141f4db16395df9f7c6c63e24d3a01
윤예준 <dbm01049@gmail.com>
2021-04-08 13:41:39 +0900

    Update Nginx conf 수정3

--- frontend/truffle/vue.config.js
+++ frontend/truffle/vue.config.js
@@ -13,7 +13,7 @@ module.exports = {
       // 프록시 요청을 보낼 api의 시작 부분
       '/truffle': {
         // 프록시 요청을 보낼 서버의 주소
-        target: 'http://localhost:8000',
+        target: 'http://localhost:8080',
       },
     },
   },
